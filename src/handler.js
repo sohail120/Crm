45046e3e16302376a3cd9ca1568c5808e2b36f56
sohail sheikh <sohail.softw@gmail.com>
2023-04-19 20:26:49 +0530

    changes

--- src/handler.js
+++ src/handler.js
@@ -39,7 +39,7 @@ exports.voiceResponse = function voiceResponse(requestBody) {
   // start.stream(url='wss://https://545e-2401-4900-52b8-7f33-e07c-c5bb-6a3e-24fd.ngrok-free.app/stream');
   start.stream({
     name: 'Example Audio Stream',
-    url: 'wss://dfde-2401-4900-52b8-7f33-3d9b-8fdd-3ed1-230f.ngrok-free.app/stream',
+    url: 'wss://https://66b6-2401-4900-57a4-caa3-ccd8-70e1-731d-5d31.ngrok-free.app/stream',
   });
   // twiml.append(start);
   // If the request to the /voice endpoint is TO your Twilio Number,
