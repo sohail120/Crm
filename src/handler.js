d0c12841a4497c5f6e3330d40fc8daef54a1953f
unknown <sohail.softw@gmail.com>
2023-06-09 17:18:56 +0530

    recording

--- src/handler.js
+++ src/handler.js
@@ -7,11 +7,11 @@ const nameGenerator = require('../name_generator');
 // const config = require('../config');
 
 var identity;
-const TWILIO_ACCOUNT_SID='ACb03fed2316821f6bedddc5b11001e8d7';
-const TWILIO_TWIML_APP_SID='AP3f9df155faea290e7d22d6be8106eb69';
-const TWILIO_CALLER_ID='+13613227906';
-const TWILIO_API_KEY='SKc7fde85dc56e5a186c380b1977e5c7e4';
-const TWILIO_API_SECRET='2HJBqodm15ClDEQbjrhomuV34NQ20QBq';
+const TWILIO_ACCOUNT_SID = 'ACb03fed2316821f6bedddc5b11001e8d7';
+const TWILIO_TWIML_APP_SID = 'AP3f9df155faea290e7d22d6be8106eb69';
+const TWILIO_CALLER_ID = '+13613227906';
+const TWILIO_API_KEY = 'SKc7fde85dc56e5a186c380b1977e5c7e4';
+const TWILIO_API_SECRET = '2HJBqodm15ClDEQbjrhomuV34NQ20QBq';
 exports.tokenGenerator = function tokenGenerator() {
   identity = nameGenerator();
 
@@ -37,12 +37,13 @@ exports.tokenGenerator = function tokenGenerator() {
 exports.voiceResponse = function voiceResponse(requestBody) {
   const toNumberOrClientName = requestBody.To;
   const callerId = TWILIO_CALLER_ID;
+  const record = 'record-from-answer-dual';
   const twiml = new VoiceResponse();
 
   // If the request to the /voice endpoint is TO your Twilio Number,
   // then it is an incoming call towards your Twilio.Device.
   if (toNumberOrClientName == callerId) {
-    const dial = twiml.dial();
+    const dial = twiml.dial({record});
 
     // This will connect the caller with your Twilio.Device/client
     dial.client(identity);
@@ -50,7 +51,7 @@ exports.voiceResponse = function voiceResponse(requestBody) {
     // This is an outgoing call
 
     // set the callerId
-    const dial = twiml.dial({callerId});
+    const dial = twiml.dial({callerId, record});
 
     // Check if the 'To' parameter is a Phone Number or Client Name
     // in order to use the appropriate TwiML noun
